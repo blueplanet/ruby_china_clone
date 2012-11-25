edce1be29ced84a761457c5d6062b722837479e8
blueplanet <erguolinge@gmail.com>
2012-11-26 07:30:32 +0900

    Init rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRubyChina::Application.config.secret_token = '9d7835520199bc8a00c7c5a71cfe1a6772c028f1b4cacb0917771046db6556faa5c805f67b40e6247869778392f21bad084c4dee610deee007c6880438521585'
