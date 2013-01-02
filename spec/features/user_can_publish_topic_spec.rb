84b75e227a0b5e04b61c6557f0cdc5ee09ff8bb7
blueplanet <erguolinge@gmail.com>
2013-01-03 07:37:28 +0900

    Refactory

--- spec/features/user_can_publish_topic_spec.rb
+++ spec/features/user_can_publish_topic_spec.rb
@@ -13,11 +13,11 @@ feature '用户希望公开帖子' do
 
     fill_in 'user_name', with: @user.name
     click_button "登录"
+
+    click_link "发布新帖"
   end
 
   scenario '访问/topics/new，应该显示新建帖子页面' do
-    click_link "发布新帖"
-
     page.should have_content "新建帖子"
     page.should have_select "topic_node_id", options: Node.all.map { |node| node.name }
     page.should have_field 'topic_title'
@@ -25,8 +25,6 @@ feature '用户希望公开帖子' do
   end
 
   scenario '输入节点、标题、内容，点击"保存"按钮，应该正常提交并显示帖子列表' do
-    click_link "发布新帖"
-
     select @ruby_node.name, from: 'topic_node_id'
     fill_in 'topic_title', with: 'new topic title'
     fill_in 'topic_content', with: 'new topic content'
